4eccf7d98b443d305fc9e290cb0bd17aec0dad7b
Frank Mitarotonda <cotton@Franks-MacBook-Pro.local>
2024-04-02 22:48:28 -0400

    Improved automation

--- googleDrive/drive_automation.py
+++ googleDrive/drive_automation.py
@@ -9,50 +9,52 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-
-def main():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("./auth/token.json"):
-    creds = Credentials.from_authorized_user_file("./auth/token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "./auth/credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("./auth/token.json", "w") as token:
-      token.write(creds.to_json())
-
-  try:
-    service = build("drive", "v3", credentials=creds)
-    parent_metadata = {
-        "name": "ParentFolder",
-        "mimeType": "application/vnd.google-apps.folder",
-    }
-    # pylint: disable=maybe-no-member
-    parent = service.files().create(body=parent_metadata, fields="id").execute()
-    parentID = parent.get("id")
-    child_metadata = {
-        "name": "ChildFolder",
-        "mimeType": "application/vnd.google-apps.folder",
-        "parents": [parentID],
-    }
-    service.files().create(body=child_metadata, fields="id").execute()
-    return parent.get("id")
-     
-  except HttpError as error:
-    print(f"An error occurred creating Folder: {error}")
-    return None
+def drive_auth():
+    """Authenticate with Drive API Project
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("./auth/token.json"):
+        creds = Credentials.from_authorized_user_file("./auth/token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+              "./auth/credentials.json", SCOPES
+          )
+        creds = flow.run_local_server(port=0)
+      # Save the credentials for the next run
+        with open("./auth/token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+def create_drive_folders():
+    """Create Drive Folders and sub-directories
+    """
+    creds = drive_auth()
+    try:
+        service = build("drive", "v3", credentials=creds)
+        parent_metadata = {
+            "name": "ParentFolder",
+            "mimeType": "application/vnd.google-apps.folder",
+        }
+        # pylint: disable=maybe-no-member
+        parent = service.files().create(body=parent_metadata, fields="id").execute()
+        parent_id = parent.get("id")
+        folder_names = ["ChildFolder1", "ChildFolder2", "ChildFolder3"]
+        for folder in folder_names:
+            child_metadata = {
+                "name": folder,
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [parent_id],
+            }
+            service.files().create(body=child_metadata, fields="id").execute()
+    except HttpError as error:
+        print(f"An error occurred creating Folder: {error}")
+        return None
 
 if __name__ == "__main__":
-  main()
+    create_drive_folders()
